6d985e690a83c32edf6c44902bb77ef2d892e023
DevStar234 <90933603+DevStar234@users.noreply.github.com>
2021-11-13 12:33:48 +0300

    Update formatDate.js

--- src/filters/formatDate.js
+++ src/filters/formatDate.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import moment from 'moment-timezone'
 import store from '../store'
-
+ 
 Vue.filter('formatDate', (value, filterFormat) => {
   const { zone, format } = store.state.app.time
  
